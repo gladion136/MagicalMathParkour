ae77ed40e2a7838f07264ba6b85740baf8728801
etienne.schmidt@stud.th-owl.de <gladion136@googlemail.com>
2021-12-03 15:59:28 +0100

    Update point sprite * little fixes

--- MINT_Spiel/objects/point.gd
+++ MINT_Spiel/objects/point.gd
@@ -1,17 +1,18 @@
 extends Node2D
 
-onready var old_pos = position
+onready var old_pos = Vector2(0, 0)
 
 func _ready():
 	$DontDespawn.start()
 
 
 func _process(delta):
-	if position != old_pos:
-		old_pos = position
-		var l_pos_x = (self.position.x - Global.coordinate_system_center.x) / Global.axis_scale
-		var l_pos_y = (self.position.y - Global.coordinate_system_center.y) / Global.axis_scale * -1
-		$Label.text = "("+str(stepify(l_pos_x, 0.01))+", "+str(stepify(l_pos_y, 0.01))+")"
+	var l_pos_x = (self.position.x - Global.coordinate_system_center.x) / Global.axis_scale
+	var l_pos_y = (self.position.y - Global.coordinate_system_center.y) / Global.axis_scale * -1
+	var cur_pos = Vector2(l_pos_x, l_pos_y)
+	if old_pos != cur_pos:
+		old_pos = cur_pos
+		$Label.text = "("+str(stepify(cur_pos.x, 0.01))+", "+str(stepify(cur_pos.y, 0.01))+")"
 
 
 func _on_Control_gui_input(event):
