674295fae55253d8da7193e5df680b7709a2e813
Etienne Schmidt <gladion136@googlemail.com>
2021-12-18 13:03:27 +0100

    RC5 * add disclaimer in webversion * add version label * fix quad function b * small fixes

--- MINT_Spiel/scenes/menu/MainMenu.gd
+++ MINT_Spiel/scenes/menu/MainMenu.gd
@@ -9,6 +9,12 @@ enum MODE {
 	MULTI
 }
 
+func _ready():
+	$VersionLbl.text = "Version: " + Global.VERSION + "-" + OS.get_name()
+	if(OS.get_name() == "HTML5"):
+		$Disclaimer.text = "Dies ist eine private Webseite für Testzwecke!\nDie Webversion kann Fehler abhängig vom Browser und Betriebssystem beinhalten."
+
+
 func _on_BtnLinear_pressed():
 	main.current_mode = MODE.LINEAR
 	main.select_menu(main.MENU.LEVELSELECTION)
